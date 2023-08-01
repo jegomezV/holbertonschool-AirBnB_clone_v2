69946ddfc1a2db012dcc0e2e951bb9ccaa00d8d7
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-01 12:04:27 -0500

    added changes for the point 5 delete object by file storange, and the function all, it can show by name of the object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            dict_return = {}
+            for key, value in FileStorage.__objects.items():
+                if key.startswith(cls.__name__):
+                    dict_return[key] = value
+                    print(dict_return[key])
+            return dict_return
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +55,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj != None:
+            try:
+                key = type(obj).__name__ + '.' + obj.id
+                del self.__objects[key]
+            except Exception:
+                print("Object no found")
