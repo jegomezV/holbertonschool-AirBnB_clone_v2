c06c4d891fae6a7b80b0f2ef3f98b99844280e6b
jegomezV <jegomezvalencia@gmail.com>
2023-08-01 13:23:46 -0500

    Setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,4 +7,3 @@ USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
